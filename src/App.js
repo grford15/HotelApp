10bf6fc38d66932bf7ec4331e91b5ecb04dbc865
Greg <g.rutherford10@gmail.com>
2020-01-15 12:59:29 +0000

    create basic navbar component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms} />
-      <Route exact path="/rooms/:slug" component={SingleRoom} />
-      <Route component={Error} />
-    </Switch>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
