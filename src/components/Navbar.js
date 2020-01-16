2ac0157b908e11f886ab8c43aebf141d534e20af
Greg <g.rutherford10@gmail.com>
2020-01-16 14:53:59 +0000

    start composing the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h4>Navbar</h4>
-      </div>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header"></div>
+        </div>
+      </nav>
     );
   }
 }
