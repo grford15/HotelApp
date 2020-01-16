f9903a32cc35dfc8304c2834015e423ad619282e
Greg <g.rutherford10@gmail.com>
2020-01-16 16:16:17 +0000

    Add hero component with seperate banner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <Hero hero="defaultHero" />;
+  return (
+    <Hero hero="defaultHero">
+      <Banner
+        title="Luxurios Rooms"
+        subTitle="Deluxe rooms starting £199"
+      >
+        <Link to="/rooms" className="btn-primary">
+          Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 Hero.defaultProps = {
