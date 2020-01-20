a59988928993740eba28f481d9461bd7281f6ee0
Greg <g.rutherford10@gmail.com>
2020-01-20 15:45:40 +0000

    add context component with RoomContext class

---
+++ src/Context.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const RoomContext = React.createContext();
+
+class RoomProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <RoomContext.Provider value={'Hello'}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export default { RoomProvider, RoomConsumer, RoomContext };
