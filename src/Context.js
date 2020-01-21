fa7d0d65f4ed7914f60ea8bee18a194dfb99b790
Greg <g.rutherford10@gmail.com>
2020-01-21 13:22:16 +0000

    retrieve data from local file & save to state

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import items from './data';
 
 const RoomContext = React.createContext();
 
@@ -6,9 +7,32 @@ class RoomProvider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      greeting: 'Hello World',
-      name: 'Greg',
+      rooms: [],
+      sortedRooms: [],
+      featuredRooms: [],
+      loading: true,
     };
+  }
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(
+        image => image.fields.file.url,
+      );
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
   }
 
   render() {
--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,8 +4,6 @@ import { RoomContext } from '../Context';
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const value = this.context;
-    console.log(value);
     return <div>Hello World</div>;
   }
 }
